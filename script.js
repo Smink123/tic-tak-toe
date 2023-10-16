cee4b2936eb23757adb59b15e408017abfb8a060
Sarah Harvey <sarahcharvey@hotmail.com>
2023-10-16 09:58:01 +0100

    set up the basic html and css styling/layout

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log('hello')
+
+//game does not start until player names are filled in
+//everything must be disabled until text boxes are filled in
+
+//when names are filled in and submitted and gameplay begins, make input/label/button disappear
+//player name title then becomes visible with inputted player name
+//also make score visible
